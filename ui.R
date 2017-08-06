95189e1b815ce924367b1f50075e147d99973c66
Jan Červený <cerveny.j@czechglobe.cz>
2017-08-06 19:02:54 -0400

    Update to secure new dplyr package compatibility
    There has been change in dplyr package in handling of summarize_each function data type. Now summarize_all should be used.

--- ui.R
+++ ui.R
@@ -4,106 +4,172 @@
 # http://shiny.rstudio.com
 #
 
+library(data.table)
+library(DT)
+library(dplyr)
+library(dtplyr)
+library(mongolite)
 library(shiny)
+library(shinyBS)
+library(tidyr)
+library(xlsx)
 
 shinyUI(fluidPage(
-   tags$head(includeScript("google-analytics.js")),
-   titlePanel("Tidy Up PBR Data", windowTitle = "Tidy Up Data"),
+   tags$head(includeScript('google-analytics.js')),
+   titlePanel('', windowTitle = 'Tidy Up Data'),
    sidebarLayout(
+      # Sidebar panel ====
       sidebarPanel(
-         fluidRow(
-            column(7, fileInput(
-               'datafile',
-               'Choose data file to upload',
-               accept = c(
-                  'application/x-zip-compressed',
-                  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                  'text/csv',
-                  'text/comma-separated-values',
-                  'text/tab-separated-values',
-                  'text/plain',
-                  '.xlsx',
-                  '.csv',
-                  '.tsv'
+         conditionalPanel(
+            condition = 'input.conditionedSidePanels==1',
+            fluidRow(
+               fileInput(
+                  'datafile',
+                  'Choose data file to upload',
+                  accept = c(
+                     'application/x-zip-compressed',
+                     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                     'text/csv',
+                     'text/comma-separated-values',
+                     'text/tab-separated-values',
+                     'text/plain',
+                     '.xlsx',
+                     '.csv',
+                     '.tsv'
+                  )
                )
-            )),
-            column(
-               5,
-               numericInput('interval', 'Averaging interval [min]', 1, 0.5, 10, 0.1, '200px'),
-               downloadButton('downloadData', 'Download')
-            )
-         ),
-         tags$hr(),
-         tags$p(
-            "For plan text data formats:"
-         ),
-         fluidRow(
-            column(4, checkboxInput('header', 'Header', TRUE)),
-            column(4, radioButtons(
-               'sep', 'Separator',
-               c(
-                  Comma = ',',
-                  Semicolon = ';',
-                  Tab = '\t'
+            ),
+            fluidRow(
+               sliderInput(
+                  'interval',
+                  'Averaging interval, min', 
+                  value = 1, min = 0.5, max = 10, step = 0.1
                ),
-               ','
-            )),
-            column(4, radioButtons(
-               'dec', 'Decimal mark',
-               c('Point' = '.',
-                 'Comma' = ','),
-               '.'
-            ))
-         ),
-         tags$hr(),
-         p(
-            "@author CzechGlobe - Department of Adaptive Biotechnologies (JaCe)"
-         ),
-         fluidRow(column(
-            12,
-            actionLink("register", label = "Register to support development of additional PBR tools"),
-            conditionalPanel(
-               "input.register",
-               textInput("name", label = "First name", value = "First name"),
-               textInput("surname", label = "Last name", value = "Last name"),
-               textInput("email", label = "Email", value = "@"),
-               textInput("organization", label = "Organization", value = "Organization"),
-               textInput("department", label = "Department", value = "Department"),
-               actionButton("send", label = "Send"),
-               p(
-                  "@email cerveny.j@czechglobe.cz"
+               # numericInput('interval', 'Interval, min', 1, min = 0.5, max = 10, step = 0.1, width = '100px'),
+               bsTooltip(
+                  'interval',
+                  'Interval that is used for lumping and averaging untidy data. Provided in minutes.',
+                  'right', options = list(container = 'body')
+               )
+            ),
+            fluidRow(
+               downloadButton('downloadData', 'Download')
+            ),
+            tags$hr(),
+            fluidRow(
+               column(
+                  width = 6,
+                  'Plain text data:'
+               ),
+               column(
+                  width = 6,
+                  checkboxInput('header', 'Header', TRUE)
+               )
+            ),
+            fluidRow(
+               column(
+                  width = 6,
+                  radioButtons(
+                     'sep',
+                     'Separator',
+                     c(
+                        Comma = ',',
+                        Semicolon = ';',
+                        Tab = '\t'
+                     ),
+                     ','
+                  )
+               ),
+               column(
+                  width = 6,
+                  radioButtons(
+                     'dec',
+                     'Decimal',
+                     c('Point' = '.',
+                     'Comma' = ','),
+                     '.'
+                  )
                )
             )
-         ))
-         ,
-         width = 5
+         ),
+         conditionalPanel(
+            condition = 'input.conditionedSidePanels==2',
+            fluidRow(
+               sliderInput(
+                  'lag',
+                  'Lag time, min', 
+                  value = 5, min = 0, max = 30, step = 1
+               ),
+               bsTooltip(
+                  'lag',
+                  'Length of lag time that defines part of data that are influenced by the dilution. Provided in minutes.',
+                  'right',
+                  options = list(container = 'body')
+               )
+            )
+         ),
+         width = 3
       ),
+      # Main panel ====
       mainPanel(
-         strong(textOutput("count")),
-         br(),
-         strong(textOutput("filename")),
-         em(textOutput("filesize")),
-         br(),
-         textOutput("inputdim"),
-         textOutput("processeddim"),
-         width = 7
+         tabsetPanel(
+            type = 'tabs',
+            tabPanel(
+               'Data Processing',
+               value = 1,
+               strong(textOutput("count")),
+               br(),
+               strong(textOutput("filename")),
+               em(textOutput("filesize")),
+               br(),
+               textOutput('inputdim'),
+               plotOutput('dataViewPlot', width = '90%')
+            ), 
+            tabPanel(
+               'Data Analysis',
+               value = 2,
+               fluidRow(
+                  column(
+                     4,
+                     br(),
+                     DT::dataTableOutput('dataProcessingTable')
+                  ),
+                  column(
+                     8,
+                     plotOutput('dataProcessingPlot')   
+                  )
+               )
+            ),
+            id = 'conditionedSidePanels'
+         ),
+         width = 9
       )
    ),
+   # Bottom panel ====
    fluidRow(
       column(
-         3,
-         tags$img(src = "img/Logo-CzechGlobe.jpg", alt = "CzechGlobe", height = 60, align = "top")
+         width = 3,
+         actionLink('register', label = 'Register to support further development'),
+         tags$p(),
+         conditionalPanel(
+            'input.register',
+            textInput("name", label = "First name", value = "First name"),
+            textInput("surname", label = "Last name", value = "Last name"),
+            textInput("email", label = "Email", value = "@"),
+            textInput("organization", label = "Organization", value = "Organization"),
+            textInput("department", label = "Department", value = "Department"),
+            actionButton("send", label = "Send"),
+            tags$hr(),
+            p(
+               '@author CzechGlobe - Department of Adaptive Biotechnologies (JaCe)'
+            ),
+            p(
+               "@email cerveny.j@czechglobe.cz"
+            )
+         )
       ),
       column(
-         2,
-         tags$img(src = "img/Logo-C4Sys.jpg", alt = "C4Sys", height = 40, align = "right")
-      ),
-      column(
-         1,
-         br()
-      ),
-      column(
-         5,
+         width = 7,
          actionLink("help", label = "Help"),
          conditionalPanel(
             "input.help",
@@ -116,7 +182,18 @@ shinyUI(fluidPage(
             p(
                "In general this tool can be used on any numeric data table, e.g. for large datasets tidying up, averaging, etc."
             )
-         )
+         ),
+         offset = 1
+      )
+   ),
+   fluidRow(
+      column(
+         2,
+         tags$img(src = "img/Logo-CzechGlobe.jpg", alt = "CzechGlobe", height = 60, align = "top")
+      ),
+      column(
+         1,
+         tags$img(src = "img/Logo-C4Sys.jpg", alt = "C4Sys", height = 45, align = "right")
       )
    )
 ))
